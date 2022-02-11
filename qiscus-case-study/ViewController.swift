fc67c33ce52ed92101e265334bb98a417bdb8ad5
galang_as <galangajisusanto@gmail.com>
2022-02-11 20:47:11 +0700

    Initial Commit

---
+++ qiscus-case-study/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  qiscus-case-study
+//
+//  Created by Galang Aji Susanto on 11/02/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
