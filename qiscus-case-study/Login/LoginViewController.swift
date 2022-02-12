e8a0ccc755eca0d2c75b2bdf60b044caf7c3db55
galang_as <galangajisusanto@gmail.com>
2022-02-12 16:16:10 +0700

    [ADD] added login view controller and implement qiscus set user

---
+++ qiscus-case-study/Login/LoginViewController.swift
@@ -0,0 +1,36 @@
+//
+//  LoginViewController.swift
+//  qiscus-case-study
+//
+//  Created by Galang Aji Susanto on 12/02/22.
+//
+
+import UIKit
+import QiscusCore
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var userIdField: UITextField!
+    @IBOutlet weak var userKeyField: UITextField!
+    @IBOutlet weak var userNameField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    @IBAction func loginButtonTapped(_ sender: UIButton) {
+        
+        guard let userName = userNameField.text, let userKey = userKeyField.text, let userId = userIdField.text else {return}
+        
+        QiscusCore.setUser(userId: userId, userKey: userKey, username: userName, avatarURL: nil, extras: nil, onSuccess: { (_) in
+
+        }) { (error) in
+            let alert = UIAlertController(title: "Failed to Login?", message: String(describing: error.message), preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "Try Again", style: .cancel, handler: nil))
+            
+            self.present(alert, animated: true)
+        }
+        
+        
+    }
+}
